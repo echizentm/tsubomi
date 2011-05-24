a761f7c656b0396f398cb3fc670e27fccf5ca876
echizentm <echizentm@158b2d50-1e92-469b-3a6f-dcbb61055aff>
2011-05-24 14:30:59 +0000

    add erika

---
+++ trunk/erika_search.cc
@@ -0,0 +1,47 @@
+// $Id$
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include "erika_louds_trie.h"
+
+using namespace std;
+using namespace erika;
+int main(int argc, char **argv) {
+  try {
+    trie *pt;
+    char *filename = argv[1];
+    if (filename[strlen(filename) - 2] == 'l') {
+      pt = new louds_trie();
+    } else {
+      pt = new basic_trie();
+    }
+    pt->read(filename);
+    cerr << "> ";
+
+
+    char *buf = NULL;
+    size_t len = 0;
+    while (getline(&buf, &len, stdin) != -1) {
+      cerr << "> ";
+      buf[strlen(buf) - 1] = '\0';
+      trie_results values;
+      pt->search(buf, values);
+      trie_results::iterator i = values.begin();
+      trie_results::iterator e = values.end();
+      while (i != e) {
+        cout << i->first << " : " << i->second << endl;
+        i++;
+      }
+      cout << "> ";
+    }
+    if (buf) { free(buf); }
+    if (pt)  { delete pt; }
+
+  } catch (const char *err) {
+    cerr << err << endl;
+    return 1;
+  }
+  return 0;
+}
+
